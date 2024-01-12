d394332b9197191c4f9a1ad270d47a12a3a7222f
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2024-01-11 20:06:07 -0600

    Se crean las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { ProveedorComponent } from './Pages/proveedor/proveedor.component';
+import { ProveedoresComponent } from './Pages/proveedores/proveedores.component';
+import { VideojuegoComponent } from './Pages/videojuego/videojuego.component';
+import { VideojuegosComponent } from './Pages/videojuegos/videojuegos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'videojuegos', pathMatch: 'full' },
+  {
+    path: 'videojuegos',
+    component: VideojuegosComponent,
+    title: 'Lista Videojuegos',
+  },
+  {
+    path: 'videojuegos/:id',
+    component: VideojuegoComponent,
+    title: 'Detalle del Videojuego',
+  },
+  {
+    path: 'proveedores',
+    component: ProveedoresComponent,
+    title: 'Lista Proveedores',
+  },
+  {
+    path: 'proveedores/:id',
+    component: ProveedorComponent,
+    title: 'Detalle del Proveedor',
+  },
+];
