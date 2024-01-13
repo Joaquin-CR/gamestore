97c343fd25f8a5dc05e021dd5fb82bec61428ee3
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2024-01-13 12:52:44 -0600

    Se hacen ajustes en la vista de videojuegos

--- src/app/Pages/videojuegos/videojuegos.component.ts
+++ src/app/Pages/videojuegos/videojuegos.component.ts
@@ -1,19 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Videojuego } from '../../../interfaces/types';
+import { ModalComponent } from '../../Components/modal/modal.component';
 import { VideojuegosService } from '../../Services/videojuegos.service';
 
 @Component({
   selector: 'app-videojuegos',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './videojuegos.component.html',
   styleUrl: './videojuegos.component.css',
+  imports: [CommonModule, ModalComponent],
 })
 export class VideojuegosComponent {
   listaVideojuegos: Videojuego[] = [];
 
-  constructor(private _videojuegoService: VideojuegosService) {}
+  constructor(
+    private _videojuegoService: VideojuegosService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this._videojuegoService.getAllVideojuegos().subscribe({
@@ -33,5 +38,9 @@ export class VideojuegosComponent {
 
   editarvideojuego(cve: number) {
     console.log('Se editará el videojuego con id:', cve);
+  }
+
+  goToVidoejuego(id: number) {
+    this.router.navigate([`videojuegos/${id}`]);
   }
 }
