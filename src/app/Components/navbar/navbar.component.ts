f3d47e9edab60001b3ade0be702c2f3df1bec5e4
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2024-01-13 12:50:53 -0600

    Se hacen ajustes en el navbar

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,10 +1,39 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  vistaActual: string = 'videojuegos';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Verifica la URL actual después de cada navegación
+        if (event.url != '/videojuegos') {
+          if (event.url.includes('proveedores')) {
+            this.vistaActual = 'proveedores';
+          } else if (event.url.includes('/videojuegos/')) {
+            this.vistaActual = 'videojuego';
+          } else if (event.url.includes('/proveedores/')) {
+            this.vistaActual = 'proveedor';
+          }
+        } else {
+          this.vistaActual = 'videojuegos';
+        }
+      }
+    });
+  }
+
+  goTo(path: string) {
+    this.router.navigate([path]);
+  }
+}
